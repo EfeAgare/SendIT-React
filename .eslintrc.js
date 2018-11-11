eaa3fc4b6949f16d1f9e498adc1df5bf2f0610df
Agare Efe Knowledge <knowledgeagare157@gmail.com>
2018-11-11 23:30:53 +0100

    [Feature #161845346]Refactoring Fetch all parcel delivery orders endpoint

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    
+    "extends": "airbnb-base",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "max-len": ["error", { "code": 80 }],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always", { "omitLastInOneLineBlock": true }
+        ],
+        "valid-jsdoc": ["error", {
+            "requireReturn": true,
+            "requireReturnType": true,
+            "requireParamDescription": false,
+            "requireReturnDescription": true,
+            "no-console": "off"
+          }],
+          "require-jsdoc": ["error", {
+              "require": {
+                  "FunctionDeclaration": true,
+                  "MethodDefinition": true,
+                  "ClassDeclaration": true,
+                  "ArrowFunctionExpression": true, 
+                  "FunctionExpression": true
+              }
+          }]
+
+    }
+};
--- server/src/models/parcels.js
+++ server/src/models/parcels.js
@@ -1,6 +1,5 @@
 const parcels = [
     {
-        id: 0,
         deliveryAddress: {
             firstName: 'Success',
             lastName: 'Shaw',
@@ -18,50 +17,8 @@ const parcels = [
         },
         email: "hrtiuo@yahoo.com",
         phoneNumber: "09070911674",
-        status: 'In Transit'
-    },
-    {
-        id: 1,
-        deliveryAddress: {
-            firstName: 'Churchill',
-            lastName: 'Paul',
-            streetAddress: '10 ake street',
-            addressCity: 'Mariga',
-            addressState: 'Niger'
-        },
-        expectedArrival: {
-            from: '2018-11-24 2:48 PM',
-            to: '2018-11-27 2:48 PM',
-        },
-        itemShipped: {
-            name: 'Xtouch E',
-            description: 'Xtouch white internal memory 8gb ',
-        },
-        email: "hrtiuo@yahoo.com",
-        phoneNumber: "09070911674",
-        status: 'Delivered'
-    },
-    {
-        id: 2,
-        deliveryAddress: {
-            firstName: 'Churchill',
-            lastName: 'Paul',
-            streetAddress: '10 ake street',
-            addressCity: 'Mariga',
-            addressState: 'Niger'
-        },
-        expectedArrival: {
-            from: '2018-11-14 2:48 PM',
-            to: '2018-11-17 2:48 PM',
-
-        },
-        itemShipped: {
-            name: 'Tecno W3',
-            description: 'Tecno W3 internal memory 8gb ',
-        },
-        email: "hrtiuo@yahoo.com",
-        phoneNumber: "09070911674",
-        status: 'Awaiting Pickup'
+        status: 'In Transit',
+        id: 0
     }
 ]
 export default parcels;
