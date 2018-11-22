74c5f2d8bde2a7a608a128176c2ba2da1c2503b4
Agare Efe Knowledge <knowledgeagare157@gmail.com>
2018-11-22 17:34:21 +0100

    test

--- server/src/tests/parcelTest.js
+++ server/src/tests/parcelTest.js
@@ -3,44 +3,29 @@ import chaiHttp from 'chai-http';
 import app from '../index';
 
 
+
 chai.use(chaiHttp);
-describe('/GET/parcels', () => {
-  it('Should Fetch a specific parcel delivery order', (done) => {
-    chai.request(app)
-      .get('/api/v1/parcels/0/')
-      .send(parcels)
+describe('/POST/auth/login', () => {
+  it('Should login a user', (done) => {
+  chai.request(app)
+  .post('/api/v1/auth/login')
+  .send(login)
       .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.typeOf(res.body, 'object', 'we have an object');
+        token = res.body.token
+        assert.equal(res.status, 201);
+        assert.typeOf(res.body.data, 'array');
         done();
       });
   });
 });
+  
 
 describe('/POST', () => {
   it('Should Create a parcel delivery order', (done) => {
     chai.request(app)
+      .set('x-access-token',token)
       .post('/api/v1/parcels/')
-      .send({
-          "firstName" : "knowledge",
-            "lastName":"kyky" , 
-            "deliveryAddress":"thkjkhvbnjmjhjckvb",
-            "deliveryLGA" :"xcfvujhbkn" ,
-            "deliveryState" :"fffdxcvjb",
-            "deliveryStreet":"ezxctvbnkjmk", 
-            "deliveryEmail" :"ert@gmail.com",
-            "deliveryPNumber":"08053456789",
-            "deliveryTime":"2011-10-10 18:40",
-            "pickUpState":"hfvjbynkm",
-            "pickUpLGA":"htcfjvbn",
-            "pickUpStreet":"gzrgxcfgvjb",
-            "pickUpPhoneNumber":"09070911675",
-            "currentLocation":"Lagos",
-            "itemDescription":"fdd hgjb",
-            "itemName":"dell",
-            "itemWeight":"56",
-            "itemQuantity":"4"
-      })
+      .send(parcelsOrder)
       .end((err, res) => {
         console.log(res.body)
         assert.equal(res.status, 200);
@@ -53,6 +38,7 @@ describe('/POST', () => {
 describe('/PUT', () => {
   it('Should Cancel a parcel delivery order', (done) => {
     chai.request(app)
+      .set('x-access-token',token)
       .put('/api/v1/parcels/1/cancel')
       .send({
 
@@ -74,7 +60,7 @@ describe('/PUT', () => {
             "itemName":"dell",
             "itemWeight":"56",
             "itemQuantity":"4",
-            status:"cancelled"        
+            status:"cancel"        
       })
       .end((err, res) => {
         console.log(res.body)
