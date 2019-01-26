7203c31199831ec8a7ed713b075a6e5648a01102
Efe <knowledgeagare157@gmail.com>
2019-01-26 15:06:54 +0100

    ch-react-config-163498058 - Configure webpack for react - Configure Babel for react - test if the Configuration works  [Finishes #163498058]

---
+++ client/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+
+class App extends Component {
+  render() {
+    return <h1>Hello Sendit</h1>;
+  }
+}
+
+export default hot(module)(App);
--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,7 @@ import expressvalidator from 'express-validator';
 import parcelRoute from './routes/parcelRoute';
 import adminRoute from './routes/adminRoute';
 import userRoute from './routes/userRoute';
-
+import path from 'path';
 
 const port = parseInt(process.env.PORT, 10) || 2000;
 const app = express();
@@ -17,43 +17,56 @@ const app = express();
 dotenv.config();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressvalidator());
-app.use(cors())
+app.use(cors());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Acess-Control-Allow-Headers', 
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization' );
-     if (req.method === 'OPTIONS') {
-         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-         return res.status(200).json({});
-    }
-    next();
-})
-app.use('/', express.static('UI'))
-app.get('/api/v1/', (req, res) =>{
-    res.status(200).json('Welcome to SendIt \n SendIT is a courier service that helps users deliver parcels to different destinations.') 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Acess-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+app.use('/', express.static('UI'));
+app.get('/api/v1/', (req, res) => {
+  res
+    .status(200)
+    .json(
+      'Welcome to SendIt \n SendIT is a courier service that helps users deliver parcels to different destinations.'
+    );
 });
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/', parcelRoute);
 app.use('/api/v1/', adminRoute);
 app.use('/api/v1/', userRoute);
 
+//react app
+app.use(express.static(path.resolve(__dirname, '../../dist')));
+
+// handling react router, for request to react app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
 app.use((req, res, next) => {
-    const error = new Error('We can\'t find the page you are looking for')
-    error.status = 404;
-    next(error)
+  const error = new Error("We can't find the page you are looking for");
+  error.status = 404;
+  next(error);
 });
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.toString()
-        }
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.toString()
+    }
+  });
 });
 
-app.listen(port, () => console.log('server running at port', port) )
+app.listen(port, () => console.log('server running at port', port));
 
 export default app;
