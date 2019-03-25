92c3c0151f48adfdef8adb6b274186e891b06360
Efe <knowledgeagare157@gmail.com>
2019-03-25 23:03:45 +0100

    feat: remove vanilla javasript

--- UI/script/index.js
+++
@@ -1,8 +0,0 @@
-function indexFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
